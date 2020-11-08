1291df47e4a70699cc7ccec6f4745f6c53069598
Spotty Bowles <skytribe@hotmail.com>
2020-11-08 13:51:58 -0800

    Addition Module For Label Printing on Brother Printer

---
+++ GiftCertificates/Models/BrotherPrinting.vb
@@ -0,0 +1,15 @@
+﻿Module BrotherPrinting
+    '//This will be the module that contains the address printing methods
+
+    Public Property Name As String = ""
+    Public Property Address1 As String = ""
+    Public Property Address2 As String = ""
+
+    Public Property City As String = ""
+    Public Property State As String = ""
+    Public Property Zip As String = ""
+
+
+
+
+End Module
